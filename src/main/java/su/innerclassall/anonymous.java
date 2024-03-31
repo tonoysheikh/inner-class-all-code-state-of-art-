d9f9851268b3d2822ba14642c1a42fc819473798
tonoysheikh <sidecharacter69@gmail.com>
2024-03-31 21:59:37 +0600

    second push

--- src/main/java/su/innerclassall/anonymous.java
+++ src/main/java/su/innerclassall/anonymous.java
@@ -1,15 +1,27 @@
 
 package su.innerclassall;
 
+import java.util.Scanner;
+
 abstract class anonymousCls{
     public abstract void myfun();
 }
 public class anonymous {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         anonymousCls an = new anonymousCls(){
            public void myfun()
            {
-               System.out.println("Hi,Print me.");
+               System.out.print("Enter your number :");
+               String num = sc.next();
+               int sum = 0;
+               for (int i = 0; i < num.length(); i++) {
+                   char c = num.charAt(i);
+                   int digit = Character.getNumericValue(c);
+                   sum += digit;
+
+               }
+               System.out.println("Here digit sum = "+sum);
            }
         };
         an.myfun();
